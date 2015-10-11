0476ab5ec85cdc6777c4ceaab0f8031c209ef536
Eric jonas <jonas@ericjonas.com>
2015-10-10 19:40:59 -0700

    now we properly build

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
+from Cython.Build import cythonize
+import numpy as np
 
 setup(name='sim2600',
       version='0.1',
       description='Simulation of Atari 2600', 
       author='Greg Jones',
       packages=['sim2600'],
+      include_dirs=[np.get_include()], 
+      ext_modules = cythonize("sim2600/mycircuitsimulator.pyx", 
+                              language='c++'), 
       package_data={'sim2600': ['chips/*', 'roms/*']},
      )
 
